fcfa425f8876632cebdcf5e4306cb75081ff7925
Rodrigo Amor <ramor@fi.uba.ar>
2023-08-16 22:50:14 -0300

    Primer commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import App from './Formulario.jsx'
+//Este componente deberia recibir por props y mostrar en pantalla la informacion
+//que envia el usuario
+
+function Card({cardInfo}) {
+  return (
+    <div className="card">
+      <h2>Card </h2>
+      <p>Nombre: {cardInfo.nombre}</p>
+      <p>Equipo favorito: {cardInfo.equipo}</p>
+    </div>
+  );
+}
+
+//Este componente deberia recibir por props y mostrar en pantalla la informacion
+//que envia el usuario
